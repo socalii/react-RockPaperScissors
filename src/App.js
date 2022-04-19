61ca0e094019d9b04ee06c6efd6c4788dbc30915
Susan Choi <2539lee@gmail.com>
2022-04-19 00:30:57 -0700

    Select Item

--- src/App.js
+++ src/App.js
@@ -8,7 +8,26 @@ import Box from './component/Box'
 // 5. 3 & 4 : decide winner
 // 6. change border color as result (win: green, lost: red, draw: black)
 
+const choice = {
+  rock: {
+    name: 'Rock',
+    img: 'https://png.pngtree.com/element_our/20190602/ourlarge/pngtree-hand-drawn-brown-stone-illustration-image_1400699.jpg',
+  },
+  paper: {
+    name: 'Paper',
+    img: 'https://media.istockphoto.com/vectors/cartoon-hand-holding-blank-piece-of-paper-illustration-vector-id1173258176',
+  },
+  scissors: {
+    name: 'Scissors',
+    img: 'https://pixy.org/src/54/thumbs350/547045.jpg',
+  },
+}
+
 function App() {
+  const play = (userChoice) => {
+    console.log('chosen', userChoice)
+  }
+
   return (
     <div>
       <div className='main'>
@@ -16,9 +35,27 @@ function App() {
         <Box title='Computer' />
       </div>
       <div className='main'>
-        <button>✌🏼</button>
-        <button>✊🏼</button>
-        <button>🖐🏼</button>
+        <button
+          onClick={() => {
+            play('rock')
+          }}
+        >
+          ✊🏼
+        </button>
+        <button
+          onClick={() => {
+            play('paper')
+          }}
+        >
+          🖐🏼
+        </button>
+        <button
+          onClick={() => {
+            play('scissors')
+          }}
+        >
+          ✌🏼
+        </button>
       </div>
     </div>
   )
