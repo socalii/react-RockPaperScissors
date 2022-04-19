2f7cd3271e208ee31e2550d66932350763304c58
Susan Choi <2539lee@gmail.com>
2022-04-19 12:53:26 -0700

    Show Winner & Styling

--- src/App.js
+++ src/App.js
@@ -27,11 +27,24 @@ const choice = {
 function App() {
   const [userSelect, setUserSelect] = useState(null)
   const [computerSelect, setComputerSelect] = useState(null)
+  const [result, setResult] = useState('')
 
   const play = (userChoice) => {
     setUserSelect(choice[userChoice])
     let computerChoice = randomChoice()
     setComputerSelect(computerChoice)
+    setResult(judgement(choice[userChoice], computerChoice))
+  }
+
+  const judgement = (user, computer) => {
+    if (user.name == computer.name) {
+      return 'draw'
+    } else if (user.name == 'Rock')
+      return computer.name == 'Scissors' ? 'win' : 'lose'
+    else if (user.name == 'Scissors')
+      return computer.name == 'Paper' ? 'win' : 'lose'
+    else if (user.name == 'Paper')
+      return computer.name == 'Rock' ? 'win' : 'lose'
   }
 
   const randomChoice = () => {
@@ -44,8 +57,8 @@ function App() {
   return (
     <div>
       <div className='main'>
-        <Box title='You' item={userSelect} />
-        <Box title='Computer' item={computerSelect} />
+        <Box title='You' item={userSelect} result={result} />
+        <Box title='Computer' item={computerSelect} result={result} />
       </div>
       <div className='main'>
         <button
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
-function Box(props) {
+const Box = (props) => {
+  let result
+  if (
+    props.title === 'Computer' &&
+    props.result !== 'draw' &&
+    props.result !== ''
+  ) {
+    result = props.result === 'win' ? 'lose' : 'win'
+  } else {
+    result = props.result
+  }
+
   return (
-    <div className='box'>
+    <div className={`box ${result}`}>
       <h1>{props.title}</h1>
       <img className='item-img' src={props.item && props.item.img} />
-      <h2>WIN</h2>
+      <h2>{result}</h2>
     </div>
   )
 }
